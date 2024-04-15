a42f3ed4837309bda9b717b30bc627b8b12de572
Namronaldo2004 <thanhdoan0910@gmail.com>
2024-04-15 21:17:55 +0700

    update BruteForce_BackTrack.py

--- Src/BruteForce_BackTrack.py
+++ Src/BruteForce_BackTrack.py
@@ -1,6 +1,5 @@
 from Src.Maps import *
 
-
 def check_state(clauses: list[list[int]], state: list[int]):
     for clause in clauses:
         check = [x in clause for x in state]
@@ -8,24 +7,24 @@ def check_state(clauses: list[list[int]], state: list[int]):
             return False
     return True
 
-
 def brute_force(clauses: list[list[int]], board: Board, *args, **kwargs) -> list[int]:
-    assigned_cell = [-flatten(x, board.cols) for x in board.assign]
+    assigned_flatten_cell = list(set([clause[0] for clause in clauses if len(clause) == 1]))
 
     def get_all_possible_states(
-        listUnassign: list[tuple[int, int]],
+        listUnassign: list[int],
         nCols: int,
         state: list[int] = [],
         index: int = 0,
     ) -> tuple[bool, list[int]]:
         if index == len(listUnassign):
             model = state.copy()
-            model += assigned_cell
+            print(model)
+            model += assigned_flatten_cell
             if check_state(clauses, model):
                 return True, model
             return False, []
 
-        literal = flatten(listUnassign[index], nCols)
+        literal = listUnassigned[index]
         for option in [-literal, literal]:
             state.append(option)
             check, model = get_all_possible_states(
@@ -36,5 +35,7 @@ def brute_force(clauses: list[list[int]], board: Board, *args, **kwargs) -> list
             state.pop()
         return False, []
 
-    check, model = get_all_possible_states(list(board.unassign), board.cols)
+    listUnassigned = list(set([i for i in range (1, board.rows * board.cols + 1)]) 
+                          - set(map(lambda x: abs(x), assigned_flatten_cell)))
+    check, model = get_all_possible_states(listUnassigned, board.cols)
     return model if check else None
