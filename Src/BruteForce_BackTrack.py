e1213c3900cca740b1a1e5a7ee50287de37e1bd2
Namronaldo2004 <thanhdoan0910@gmail.com>
2024-04-15 21:56:03 +0700

    fix sth

--- Src/BruteForce_BackTrack.py
+++ Src/BruteForce_BackTrack.py
@@ -18,11 +18,10 @@ def brute_force(clauses: list[list[int]], board: Board, *args, **kwargs) -> list
     ) -> tuple[bool, list[int]]:
         if index == len(listUnassign):
             model = state.copy()
-            print(model)
             model += assigned_flatten_cell
             if check_state(clauses, model):
                 return True, model
-            return False, []
+            return False, None
 
         literal = listUnassigned[index]
         for option in [-literal, literal]:
@@ -33,9 +32,11 @@ def brute_force(clauses: list[list[int]], board: Board, *args, **kwargs) -> list
             if check:
                 return True, model
             state.pop()
-        return False, []
+        return False, None
 
     listUnassigned = list(set([i for i in range (1, board.rows * board.cols + 1)]) 
                           - set(map(lambda x: abs(x), assigned_flatten_cell)))
     check, model = get_all_possible_states(listUnassigned, board.cols)
     return model if check else None
+
+
